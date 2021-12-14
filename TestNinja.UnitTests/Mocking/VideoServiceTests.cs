1a2900ec76d6c7e0405aa7b8aa7907762a896cfb
Dragica Čolak <dragica.colak@infodom.hr>
2021-12-14 17:18:25 +0100

    using moq framework

---
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class VideoServiceTests
+    {
+        private Mock<IFileReader> _fileReader;
+        private VideoService _videoService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileReader = new Mock<IFileReader>();
+            _videoService = new VideoService(_fileReader.Object);
+        }
+
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnErrorMessage()
+        {
+            _fileReader.Setup(x => x.Read("video.txt")).Returns("");
+            var result = _videoService.ReadVideoTitle();
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
