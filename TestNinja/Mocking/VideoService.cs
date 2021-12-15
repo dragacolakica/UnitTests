61700c460a017ad4923ca9e49db2939655c94de9
Dragica Čolak <dragica.colak@infodom.hr>
2021-12-15 14:42:29 +0100

    moq external dependencies

--- TestNinja/Mocking/VideoService.cs
+++ TestNinja/Mocking/VideoService.cs
@@ -8,10 +8,12 @@ namespace TestNinja.Mocking
 {
     public class VideoService
     {
-        private IFileReader _fileReader;    
-        public VideoService(IFileReader fileReader = null)
+        private IFileReader _fileReader;
+        private IVideoRepository _repository;
+        public VideoService(IFileReader fileReader = null, IVideoRepository repository = null)
         {
-            _fileReader = fileReader ?? new FileReader();  
+            _fileReader = fileReader ?? new FileReader();
+            _repository = repository ?? new VideoRepository();
         }
         public string ReadVideoTitle()
         {
@@ -25,19 +27,13 @@ namespace TestNinja.Mocking
         public string GetUnprocessedVideosAsCsv()
         {
             var videoIds = new List<int>();
+            var videos = _repository.GetUnprocessedVideos();
 
-            using (var context = new VideoContext())
-            {
-                var videos =
-                    (from video in context.Videos
-                     where !video.IsProcessed
-                     select video).ToList();
+            foreach (var v in videos)
+                videoIds.Add(v.Id);
 
-                foreach (var v in videos)
-                    videoIds.Add(v.Id);
+            return String.Join(",", videoIds);
 
-                return String.Join(",", videoIds);
-            }
         }
     }
 
